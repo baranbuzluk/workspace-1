0857d40e79beedf135444baa151739a4a9890ecd
baran <baranbuzluk@gmail.com>
2020-10-19 13:17:40 +0300

    matlab-ders2

---
+++ matlab/matlab_dersleri/ders2.m
@@ -0,0 +1,14 @@
+%%1.Bolum Operatorler
+%()
+%^
+%*/
+%+-
+clear
+matris1=[1 2 3;
+         5 6 7;
+         8 9 10];
+matris2=[9 8 7;
+         6 5 4;
+         3 2 1];
+matris3=matris1.*matris2 % birebir çarpım
+matris4=matris1*matris2 %% matris çarpımı
